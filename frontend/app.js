aae4e02482045fd80c9feea69334777ac5872146
Gehrig Chao <gchao@gehrigs-mbp.dhcp.nd.edu>
2018-11-07 17:41:59 -0500

    displays backend card data in frontend card view

--- frontend/app.js
+++ frontend/app.js
@@ -1,16 +1,19 @@
 var myApp = angular.module('myApp',[]) ;
 
-myApp.controller('mainController',['$scope', function($scope) {
+myApp.controller('mainController',['$scope', '$http', function($scope,$http) {
     
-    $scope.cards=[];
-    $scope.cards[0]={location:"Chicago, IL", price:"70", startTime:"10:00PM", link:"https://images.unsplash.com/photo-1524168272322-bf73616d9cb5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=161ce6b10a1b9518237d89cc7510a018&auto=format&fit=crop&w=3300&q=80", info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu."};
-    
-    $scope.cards[1]={location:"New York City, NY", price:"50", startTime:"8:00PM", link:"https://images.pexels.com/photos/450597/pexels-photo-450597.jpeg", info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu."};
-    
-    $scope.cards[2]={location:"Los Angeles, CA", price:"70", startTime:"8:00PM", link:"https://d15gqlu8dfiqiu.cloudfront.net/s3fs-public/styles/banner/public/images/chapters/Los-Angeles-Travel-Massive.jpg", info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu."};
-    
-    $scope.cards[3]={location:"Orlando, FL", price:"81", startTime:"8:00PM", link:"https://goairportshuttle.com/images/site/slider-orlando-1.jpg", info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu."};
-    
-    $scope.cards[4]={location:"Dallas, TX", price:"23", startTime:"8:00PM", link:"https://miro.medium.com/max/732/1*hTClRaGFf9tMnBCKyx4G_w.jpeg", info:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu."};
+    $http({
+  method: 'GET',
+  url: 'http://localhost:3000/gruber'
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+        console.log(response);
+        $scope.cards=response.data;
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+ 
     
 }])
