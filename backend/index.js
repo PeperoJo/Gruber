8fa03292835f9b461c98150f7165b10f5db980b8
Shuai He <hs.heshuai@gmail.com>
2018-11-20 10:44:22 -0500

    changes made to Uber authentication

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,23 @@ const express = require('express');
 //define variables
 const app = express();
 var Uber = require('node-uber');
-var Uber = new Uber({
-    client_id: '2qgPXqUuIBG-hy4B--7Kpnj3g439mHZc',
-  client_secret: 'pQolP_kaEzXqvyyYZbamK1oCUGga9uZ1Zp_LXAxU',
-  server_token: 'V5QIaLdSHE2GYSzn1xL-ZYc0yVnlYkFIAiYJkDYl',
+
+var uber = new Uber({
+  client_id: '35x43Hkw7jfYO8428ecVsUhh5zZ3s8Sf',
+  client_secret: 'sC7nI48mzsQcJd82OmXK9zkxDFK8VlsRyzGMvRck',
+  server_token: 'HvU3swHGbI0SfwGrYzjHaQYHhv8Y0bwF7n-b9veP',
   redirect_uri: 'http://localhost:3000',
-  name: 'Grober', //Uber does not allow project names to have the word "uber" in them
+  name: 'Groberr', //Uber does not allow project names to have the word "uber" in them
   language: 'en_US', // optional, defaults to en_US
   sandbox: true, // optional, defaults to false
-  proxy: 'PROXY URL' // optional, defaults to none
+  // proxy: 'PROXY URL' // optional, defaults to none
 });
 
-var authURL = uber.getAuthorizeUrl(['history','profile','request','places']);
+app.get('/api/login', function(request, response) {
+  // alert("wassup");
+  var url = uber.getAuthorizeUrl(['history','profile', 'request', 'places']);
+  response.redirect(url);
+});
 
 const cards = [
         {id: 0,'location': 'Chicago, IL','price': '1','time': '10:00 PM', 'link': 'https://images.unsplash.com/photo-1524168272322-bf73616d9cb5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=161ce6b10a1b9518237d89cc7510a018&auto=format&fit=crop&w=3300&q=80',
@@ -28,6 +33,7 @@ const cards = [
         {id: 4,'location': 'Dallas, TX','price': '5','time': '8:00 PM', 'link':'https://miro.medium.com/max/732/1*hTClRaGFf9tMnBCKyx4G_w.jpeg',
         'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
 ]
+
 //middleware
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
