646c7417cf5b02ba4c081d032f233e1e79a63c19
Shuai He <hs.heshuai@gmail.com>
2019-01-23 18:06:45 -0500

    Parse db tested

--- backend/testParse.js
+++ backend/testParse.js
@@ -7,19 +7,35 @@ Parse.initialize(
   '2C5w4GFdMvThWCeKnvuF4IsR4mJqXCmy8IXH9R6d' // This is your Master key (never use it in the frontend)
 );
 
-const MyCustomClass = Parse.Object.extend('MyCustomClassName');
-const myNewObject = new MyCustomClass();
+const Cards = Parse.Object.extend('Cards');
 
-myNewObject.set('myCustomKey1Name', 'myCustomKey1Value');
-myNewObject.set('myCustomKey2Name', 'myCustomKey2Value');
+const input = [
+        {id: 0,'location': 'Chicago, IL','price': '1','time': '10:00 PM', 'link': 'https://images.unsplash.com/photo-1524168272322-bf73616d9cb5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=161ce6b10a1b9518237d89cc7510a018&auto=format&fit=crop&w=3300&q=80',
+        'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
+        {id: 1,'location': 'New York City, NY','price':'1' ,'time': '8:00 PM', 'link': 'https://images.pexels.com/photos/450597/pexels-photo-450597.jpeg'
+        ,'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
+        {id: 2,'location': 'Los Angeles, CA','price': '2','time': '8:00 PM', 'link': 'https://d15gqlu8dfiqiu.cloudfront.net/s3fs-public/styles/banner/public/images/chapters/Los-Angeles-Travel-Massive.jpg'
+        ,'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
+        {id: 3,'location': 'Orlando, FL','price': '3','time': '8:00 PM', 'link':'https://goairportshuttle.com/images/site/slider-orlando-1.jpg',
+        'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
+        {id: 4,'location': 'Dallas, TX','price': '5','time': '8:00 PM', 'link':'https://miro.medium.com/max/732/1*hTClRaGFf9tMnBCKyx4G_w.jpeg',
+        'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
+]
 
-myNewObject.save().then(
-  (result) => {
-    if (typeof document !== 'undefined') document.write(`ParseObject created: ${JSON.stringify(result)}`);
-    console.log('ParseObject created', result);
-  },
-  (error) => {
-    if (typeof document !== 'undefined') document.write(`Error while creating ParseObject: ${JSON.stringify(error)}`);
-    console.error('Error while creating ParseObject: ', error);
-  }
-);
+for (i = 0; i<input.length; i++){
+      const myNewCard = new Cards();
+      Object.keys(input[i]).forEach(function(key) {
+        myNewCard.set(key, input[i][key]);
+      })
+
+      myNewCard.save().then(
+        (result) => {
+          if (typeof document !== 'undefined') document.write(`ParseObject created: ${JSON.stringify(result)}`);
+          console.log('ParseObject created', result);
+        },
+        (error) => {
+          if (typeof document !== 'undefined') document.write(`Error while creating ParseObject: ${JSON.stringify(error)}`);
+          console.error('Error while creating ParseObject: ', error);
+        }
+      );
+}
