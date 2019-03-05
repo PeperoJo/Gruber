9c4c30fd40503c28aa517423eacceb2670dd861a
Shuai He <hs.heshuai@gmail.com>
2019-03-04 21:41:06 -0500

    Add working code for drawing marker from Parse

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,104 @@ const app = express();
 var Uber = require('node-uber');
 const opencage = require('opencage-api-client');
 
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Gmail API.
+  authorize(JSON.parse(content), listLabels);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  gmail.users.labels.list({
+    userId: 'me',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const labels = res.data.labels;
+    if (labels.length) {
+      console.log('Labels:');
+      labels.forEach((label) => {
+        console.log(`- ${label.name}`);
+      });
+    } else {
+      console.log('No labels found.');
+    }
+  });
+}
+
+//middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
 
 var uber = new Uber({
   client_id: '2qgPXqUuIBG-hy4B--7Kpnj3g439mHZc',
@@ -29,7 +127,8 @@ app.get('/location/address', function(request, response) {
         console.log(place.formatted);
         console.log(place.geometry);
         console.log(place.annotations.timezone.name);
-        response.redirect('/location/coordinates?lat='+ place.geometry.lat + '&lng=' + place.geometry.lng);
+//        response.redirect('/location/coordinates?lat='+ place.geometry.lat + '&lng=' + place.geometry.lng);
+		  response.send(place.geometry);
       }
     } else if (data.status.code == 402) {
       console.log('hit free-trial daily limit');
@@ -103,13 +202,6 @@ const cards = [
         'info':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, eros quis vulputate convallis, lectus mauris ornare tortor, vel malesuada arcu diam id eros. Vestibulum quis purus et felis eleifend auctor. Proin sed eros feugiat, faucibus ipsum sit amet, sagittis dolor. Aenean posuere leo in faucibus congue. In hac habitasse platea dictumst. Etiam in nisi elementum, maximus felis eu, cursus nisi. Quisque ac commodo neque. Fusce elit risus, pulvinar ac magna eu, sagittis vestibulum arcu.'},
 ]
 
-//middleware
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
 //body
 app.get('/gruber', (req, res) => {
     res.send(cards);
--- frontend/app.js
+++ frontend/app.js
@@ -1,55 +1,70 @@
-var myApp = angular.module('myApp',['ngParse', 'ngRoute', 'ngMaterial', 'mapboxgl-directive']);
+var myApp = angular.module('myApp', ['ngParse', 'ngRoute', 'ngMaterial', 'mapboxgl-directive']);
 
 /* Trip Model */
-var TripModel = function(Parse) {
-    this.Parse = Parse;
-    this.data = {};
-    this.collection = [];
-    this.name = 'Trip';
-    this.New = New;
-    this.getById = getById;
-    this.getByName = getByName;
-    this.getByDest = getByDest;
+var TripModel = function (Parse) {
+	this.Parse = Parse;
+	this.data = {};
+	this.collection = [];
+	this.name = 'Trip';
+	this.New = New;
+	this.getById = getById;
+	this.getByName = getByName;
+	this.getByDest = getByDest;
+	this.getByDestAll = getByDestAll;
 	this.addTrip = addTrip;
+
+	function New(obj) {
+		if (angular.isUndefined(obj)) {
+			const parseObject = new this.Parse.Object(this.name);
+			return parseObject;
+		} else {
+			return obj;
+		}
+	}
+
+	function getById(id) {
+		return new this.Parse.Query(this.New()).get(id)
+			.then(result => {
+				this.Parse.defineAttributes(result, this.fields);
+				this.data = result;
+				return Promise.resolve(result);
+			}).catch(error => Promise.reject(error));
+	}
+
+	function getByName(name) {
+		return new this.Parse.Query(this.New())
+			.equalTo('name', name)
+			.first()
+			.then(result => {
+				this.Parse.defineAttributes(result, this.fields);
+				this.data = result;
+				console.log('result', result)
+				return result;
+			})
+	}
+
+	function getByDest(dest) {
+		return new this.Parse.Query(this.New())
+			.equalTo('destination', dest)
+			.first()
+			.then(result => {
+				this.data = result;
+				console.log(result);
+				return result;
+			})
+	}
+
+	function getByDestAll(dest) {
+		return new this.Parse.Query(this.New())
+			.equalTo('destination', dest)
+			.find()
+			.then(result => {
+				this.data = result;
+				return result;
+			})
+	}
+
 	
-    function New(obj) {
-        if (angular.isUndefined(obj)) {
-            const parseObject = new this.Parse.Object(this.name);
-            return parseObject;
-        } else {
-            return obj;
-        }
-    }
-    function getById(id) {
-        return new this.Parse.Query(this.New()).get(id)
-            .then(result => {
-                this.Parse.defineAttributes(result, this.fields);
-                this.data = result;
-                return Promise.resolve(result);
-            }).catch(error => Promise.reject(error));
-    }
-    function getByName(name) {
-         console.log('name', name)
-         return new this.Parse.Query(this.New())
-             .equalTo('name', name)
-             .first()
-             .then(result => {
-                 this.Parse.defineAttributes(result, this.fields);
-                 this.data = result;
-                 console.log('result', result)
-                 return result;
-             })
-     }
-    function getByDest(dest) {
-         return new this.Parse.Query(this.New())
-             .equalTo('destination', dest)
-             .first()
-             .then(result => {
-                 this.Parse.defineAttributes(result, this.fields);
-                 this.data = result;
-                 return result;
-             })
-     }
 	function addTrip(trip) {
 		var newTrip = this.New();
 		console.log(this.data);
@@ -64,102 +79,228 @@ var TripModel = function(Parse) {
 			(error) => {
 				console.error('Error while creating ParseObject: ', error);
 			}
-		);		
+		);
 	}
-    //     class getAllAgencies() {
-    //         return new this.Parse.Query(this.New()).find(agencies => {
-    //             agencies.forEach(agency =>
-    //                 this.Parse.defineAttributes(agency, this.fields)
-    //             );
-    //             this.collection = agencies;
-    //             return Promise.resolve(agencies);
-    //         }).catch(error => Promise.reject(error));
-    //     }
-    
+
+	//     class getAllAgencies() {
+	//         return new this.Parse.Query(this.New()).find(agencies => {
+	//             agencies.forEach(agency =>
+	//                 this.Parse.defineAttributes(agency, this.fields)
+	//             );
+	//             this.collection = agencies;
+	//             return Promise.resolve(agencies);
+	//         }).catch(error => Promise.reject(error));
+	//     }
+
 }
+
+var GeoDecoder = function($http) {
+	this.getCoordsfromAddr = getCoordsfromAddr;
+	function getCoordsfromAddr(location) {
+		return $http({
+			method: 'GET',
+			url: 'http://localhost:3000/location/address',
+			params: {
+				address: location
+			}
+		})
+	}
+}
+
 TripModel
-    .$inject = ['Parse'];
+	.$inject = ['Parse'];
+GeoDecoder
+	.$inject = ['$http'];
 myApp
-    .service('TripModel', TripModel);
+	.service('TripModel', TripModel);
+myApp
+	.service('GeoDecoder', GeoDecoder);
 
 angular.module('myApp')
-  .config(['ParseProvider', function(ParseProvider) {
-    var MY_PARSE_APP_ID = 'mzhOlXatBmYQSNUS1DM8qkFtWyUzUNPqCTtvS3Jk';
-    var MY_PARSE_JS_KEY = 'fuErVJGUuOmR0lFKJYaycPfEPnNxckNB7eyLmpwp';
-    ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
-    ParseProvider.serverURL = 'https://parseapi.back4app.com';
+	.config(['ParseProvider', function (ParseProvider) {
+		var MY_PARSE_APP_ID = 'mzhOlXatBmYQSNUS1DM8qkFtWyUzUNPqCTtvS3Jk';
+		var MY_PARSE_JS_KEY = 'fuErVJGUuOmR0lFKJYaycPfEPnNxckNB7eyLmpwp';
+		ParseProvider.initialize(MY_PARSE_APP_ID, MY_PARSE_JS_KEY);
+		ParseProvider.serverURL = 'https://parseapi.back4app.com';
   }]);
 
-angular.module('myApp').config(['$locationProvider', function($locationProvider) {
-  $locationProvider.hashPrefix('');
+angular.module('myApp').config(['$locationProvider', function ($locationProvider) {
+	$locationProvider.hashPrefix('');
 }]);
 
-angular.module('myApp').config(function($routeProvider){    
-    $routeProvider
-    .when('/', {
-        templateUrl: 'pages/home.html',
-        controller: 'mainController'
-    })
-    .when('/add', {
-        templateUrl: 'pages/form.html',
-        controller: 'secondController'            
-    })
-    .when('/dest', {
-        templateUrl: 'pages/destination.html',
-        controller: 'mapController'            
-    })
+angular.module('myApp').config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
+		.when('/add', {
+			templateUrl: 'pages/form.html',
+			controller: 'secondController'
+		})
+		.when('/confirm', {
+			templateUrl: 'pages/confirm.html',
+			controller: 'secondController'
+		})
+		.when('/friends', {
+			templateUrl: 'pages/friends.html',
+			controller: 'friendController'
+		})
+		.when('/dest', {
+			templateUrl: 'pages/destination.html',
+			controller: 'mapController'
+		})
 })
 
 /* Controller */
-myApp.controller('mainController',['$scope', '$http', 'TripModel',function($scope,$http,TripModel) {
-    
-    $scope.currentNavItem = 'Gruber';
+myApp.controller('mainController', ['$scope', '$http', 'TripModel', function ($scope, $http, TripModel) {
 
-    $scope.goto = function(page) {
-      $scope.status = "Goto " + page;
-    };
+	$scope.currentNavItem = 'Gruber';
 
-//   $http({
-//   method: 'GET',
-//   url: 'http://localhost:3000/gruber'
-// }).then(function successCallback(response) {
-//     // this callback will be called asynchronously
-//     // when the response is available
-//         console.log(response);
-//         $scope.cards=response.data;
-//   }, function errorCallback(response) {
-//     // called asynchronously if an error occurs
-//     // or server returns response with an error status.
-//   });
- 
+	$scope.goto = function (page) {
+		$scope.status = "Goto " + page;
+	};
+
+	//   $http({
+	//   method: 'GET',
+	//   url: 'http://localhost:3000/gruber'
+	// 	}).then(function successCallback(response) {
+	//     // this callback will be called asynchronously
+	//     // when the response is available
+	//         console.log(response);
+	//         $scope.cards=response.data;
+	//   }, function errorCallback(response) {
+	//     // called asynchronously if an error occurs
+	//     // or server returns response with an error status.
+	//   });
+	// 
 }])
 
-myApp.controller('secondController', ['$scope', 'TripModel', function ($scope, TripModel) {
-    $scope.getFormData = function() {
-        alert("submitted");
-		console.log($scope.user);
-		TripModel.addTrip($scope.user);
-    }
-    
-    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-        'WY').split(' ').map(function(state) {
-            return {abbrev: state};
-    });
-	console.log($scope.user);
+myApp.controller('secondController', ['$scope', '$window', 'TripModel', function ($scope, $window, TripModel) {
+	$scope.getFormData = function () {
+		alert("submitted");
+		console.log($scope.trip);
+		TripModel.addTrip($scope.trip);
+		$window.location.href = '#/confirm';
+	}
+
+	$scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+		'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+		'WY').split(' ').map(function (state) {
+		return {
+			abbrev: state
+		};
+	});
 }]);
 
 myApp.run([function () {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZ3J1YmVybmQiLCJhIjoiY2pydHVyamE0MDJpdDQ0b2gxOHlqM2VtaiJ9.KdzYKQORKLwPfgATFxYIZw';
+	mapboxgl.accessToken = 'pk.eyJ1IjoiZ3J1YmVybmQiLCJhIjoiY2pydHVyamE0MDJpdDQ0b2gxOHlqM2VtaiJ9.KdzYKQORKLwPfgATFxYIZw';
+
 }])
 
-myApp.controller('mapController', ['$scope', function ($scope) {
-        $scope.glControls = {
-          navigation: {
-            enabled: true,
-            options: {
-              position: 'top-left'
-            }
-          }
-        };
+myApp.controller('mapController', ['$scope', '$http', 'TripModel','GeoDecoder', function ($scope, $http, TripModel, GeoDecoder) {
+		
+	//Get all the addresses objects from Parse given a destiation
+	var getListOfAddrObj = TripModel.getByDestAll("Chicago").then(function (result) {
+		return result;
+	});
+
+	//Return the coordinates array needed for map rendering
+	var coordsArr = getListOfAddrObj.then(function(addrObjects){
+		var coordsPromises = []
+		addrObjects.forEach(function (addrObject){
+//			addrObject.set("finished", true);
+//			console.log(addrObject);
+			coordsPromises.push(translateAddrToCoord(addrObject));
+		});
+		Promise.all(coordsPromises).then(data => {
+			console.log(data);
+			drawMarkers(data);
+		});
+	});													 
+
+	function translateAddrToCoord(addr) {
+		var addr_formatted = formatAddr(addr);
+		return GeoDecoder.getCoordsfromAddr(addr_formatted);
+	}
+								   
+	function formatAddr(addr) {
+		var addr_formatted = addr.attributes.address + ',' + addr.attributes.city + ',' + addr.attributes.state;
+		return addr_formatted;
+	}
+	
+	function createElement(iconSize) {
+		var element = document.createElement('div');
+		element.style.backgroundImage = 'url(https://placekitten.com/g/' + iconSize.width + '/' + iconSize.height + '/)';
+		element.style.width = iconSize.width + 'px';
+		element.style.height = iconSize.height + 'px';
+		element.style.borderRadius = '50%';
+
+		return element;
+	}
+
+	$scope.glControls = {
+		navigation: {
+			enabled: true,
+			options: {
+				position: 'top-left'
+			}
+		}
+	};
+	
+	function drawMarkers(coordArray) {
+		$scope.glMarkers = [];
+		coordArray.forEach(function (coord) {
+			$scope.glMarkers.push({
+				element: createElement({
+					width: 35,
+					height: 35
+				}),
+				popup: {
+					enabled: true,
+					message: 'Hello World',
+					options: {
+						offset: 20
+					}
+				},
+				coordinates: [coord.data.lng, coord.data.lat]
+			});
+		});
+	}
+	
+}]);
+
+myApp.controller('friendController', ['$scope', function ($scope) {
+	var imagePath = 'img/list/60.jpeg';
+	$scope.messages = [{
+		face: imagePath,
+		what: 'Chicago this weekend?',
+		who: 'Gary',
+		when: '3:08PM',
+		notes: " Let's go! "
+    }, {
+		face: imagePath,
+		what: 'Downtown bar this weekend?',
+		who: 'Shuai',
+		when: '3:08PM',
+		notes: " It's beer thirty! "
+    }, {
+		face: imagePath,
+		what: 'Brunch this weekend?',
+		who: 'Jordan',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+    }, {
+		face: imagePath,
+		what: 'Brunch this weekend?',
+		who: 'Kobe',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+    }, {
+		face: imagePath,
+		what: 'Brunch this weekend?',
+		who: 'James',
+		when: '3:08PM',
+		notes: " I'll be in your neighborhood doing errands"
+    }];
 }]);
